4357fe66d9800644ca59e9ec9e4252f8844789d5
José Támara <jochitoel1@hotmail.com>
2018-02-28 00:57:25 -0500

    Add files via upload

---
+++ src/containers/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SearchBar from './search_bar'
+
+export default class Home extends Component {
+	render() {
+		return (
+			<div>
+			Hi Támara!
+				<SearchBar />
+			</div>
+			)
+	}
+}
