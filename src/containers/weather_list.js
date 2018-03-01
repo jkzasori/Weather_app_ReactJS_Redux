fe0724e4dcca46bcd4f17628cf902e657ffea2ce
José Támara <jochitoel1@hotmail.com>
2018-03-01 04:40:19 -0500

    Add files via upload

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
 
  class WeatherList extends Component {
  	renderWeather(cityData){//contains the city data >list>main>etc...
  		const name = cityData.city.name;
+ 		const temps = cityData.list.map(weather => weather.main.temp);//manipulating the api, we get the list and map over it to get the temp. {main: {temp: 260, humidity: 40, pressure: 55}} is the argument we pass as weathe
+ 		console.log(temps)
  		return(
  			<tr key={name}>
  				<td>{name}</td>
+ 				<td>
+ 					<Chart data={temps} color="orange" />
+ 				</td>
  			</tr>
  			)
  	}
